35d96b9bbb1b4885a784039664c0e728ef950f12
David Mathew <damathew@sep.com>
2011-11-08 13:07:22 -0500

    commit initial version

---
+++ jquery.ui.autocomplete.listcssclass.js
@@ -0,0 +1,24 @@
+/*
+ * jQuery UI Autocomplete HTML Extension
+ *
+ * Copyright 2011, David Mathew (http://usabilitydistilled.com)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * http://github.com/mathewda/jquery-ui-autocomplete-customcss
+ */
+
+(function( $ ) {
+
+    var proto = $.ui.autocomplete.prototype,
+        renderMenu = proto._renderMenu;
+
+    $.extend( proto, {
+        _renderMenu: function(ul, items) {
+            if (this.options.listCssClass) {
+                $(ul).addClass(this.options.listCssClass);
+            }
+            renderMenu.apply(this, arguments);
+        }
+    });
+
+})( jQuery );
